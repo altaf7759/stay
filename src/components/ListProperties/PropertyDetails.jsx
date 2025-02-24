5e6a802e050657d3dd1cc626fe9adcbb4c88b74f
Altaf Raja <altafraja7369892024.com>
2025-02-24 21:43:21 +0530

    ShowBtns wrong import in PropertyDetails.jsx

--- src/components/ListProperties/PropertyDetails.jsx
+++ src/components/ListProperties/PropertyDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import ShowBtns from "./showBtns";
+import ShowBtns from "./ShowBtns";
 import { useDispatch, useSelector } from "react-redux";
 import { setPropertyDetails } from "../../faetures/PropertyForm/PropertySlice";
 import { setStep } from "../../faetures/PropertyForm/stepSlice";
